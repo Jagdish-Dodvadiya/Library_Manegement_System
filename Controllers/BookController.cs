81111f6117bdf00ac8973725d4d7939145eb388f
Jagdish-Dodvadiya <mrthakor575@gmail.com>
2026-04-07 11:47:03 +0530

    Initial commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,103 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext Context)
+        {
+            _context = Context;
+        }
+
+        public IActionResult Index()
+        {
+            var book = _context.Books.ToList();
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Update(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet, ActionName("Get")]
+        public IActionResult Details(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+        
+        public IActionResult Details()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Author { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
